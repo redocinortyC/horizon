688015513183c6d80cb787ce0c5a030beaf0bf61
Zeyu Yao <novodoodle@gmail.com>
2022-07-12 11:55:30 +0800

    Add default cooldown for commands

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,10 @@
+const { Collection } = require("discord.js");
 const client = require("../utils/client");
 const chalk = require("chalk");
+
+// Cooldown handler
+const cooldowns = new Collection();
+const duration = 10000;
 
 // On interaction create event 
 module.exports = {
@@ -10,8 +15,23 @@ module.exports = {
         if (!command) return;
 
         try {
-            console.log(chalk.magenta("[INTERACTION] %s executed the %s command in #%s."), interaction.user.tag, interaction.commandName, interaction.channel.name);
-            await command.execute(interaction);
+            const cooldown = {
+                user: interaction.user.id,
+                command: interaction.commandName,
+            }
+
+            if (cooldowns.has(cooldown.user + cooldown.command)) {
+                console.log(chalk.yellow("[INTERACTION] %s is on cooldown for %s."), interaction.user.tag, interaction.commandName);
+                interaction.reply(`You can only use this command once every ${duration / 1000} seconds.`);
+                return;
+            } else {
+                console.log(chalk.magenta("[INTERACTION] %s executed the %s command in #%s."), interaction.user.tag, interaction.commandName, interaction.channel.name);
+                await command.execute(interaction);
+                cooldowns.set(cooldown.user + cooldown.command);
+                setTimeout(() => {
+                    cooldowns.delete(cooldown.user + cooldown.command);
+                }, duration);
+            }
         } catch (error) {
             console.error(error);
             await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
